cc4484ec00347c11a3e21641ca1bd33c7d87ba6e
alvin <alvinsunyixiao@gmail.com>
2018-03-17 19:56:31 -0500

    add rune header

---
+++ cvtoolbox/rune.h
@@ -0,0 +1,17 @@
+#ifndef _DETECTION_
+#define _DETECTION_
+
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+
+#include <opencv2/opencv.hpp>
+#include <stdlib.h>
+#include <vector>
+
+using namespace cv;
+using namespace std;
+
+
+
+#endif
