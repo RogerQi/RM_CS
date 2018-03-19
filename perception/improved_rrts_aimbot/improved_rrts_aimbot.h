0d04ef1e14f7edf5c891a42e6152b266824346a0
Roger Q <twofs0.5@gmail.com>
2018-03-18 20:31:26 -0500

    add doxygen comments

--- perception/improved_rrts_aimbot/improved_rrts_aimbot.h
+++ perception/improved_rrts_aimbot/improved_rrts_aimbot.h
@@ -41,22 +41,33 @@ private:
     int my_distillation_threshold;
 
     /**
-    *
+    * @brief naive model of detecting light bars with findcontours
+    * @param distilled_color
+    * @param gray_bin binarized gray img
+    * @return vector of cv::ROtatedRect objects
     */
     vector<RotatedRect> detect_lights(Mat & distilled_color, Mat & gray_bin);
 
     /**
-    *
+    * @brief naive filtering model
+    * @param ori_img original image captured by camera
+    * @param detect_lights detected light bars pulled from last step
+    * @return vector of cv::ROtatedRect objects
     */
     vector<RotatedRect> filter_lights(const Mat & ori_img, const vector<RotatedRect> & detected_light);
 
     /*
-    *
+    * @brief naive armor detecting model based on armor angle, size, aspect ratio, light bar relative angles
+    * @param filtered_light_bars filtered light bars
+    * @param ori_img ref to original image captured by camera
+    * @return vector of armor_loc objects
     */
     vector<armor_loc> detect_armor(vector<RotatedRect> & filtered_light_bars, const Mat & ori_img);
 
     /*
-    *
+    * @brief armor filtering model. Not implemented for now
+    * @param armor_obtained armor_loc objects from last step
+    * @return filtered armor_loc objects
     */
     vector<armor_loc> filter_armor(const vector<armor_loc> & armor_obtained);
 };
