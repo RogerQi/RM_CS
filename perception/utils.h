2f02c85bd8a89c7d06aa6efce9d009bbd39293ee
Roger Q <twofs0.5@gmail.com>
2018-07-13 18:04:30 +0800

    add timer util

---
+++ perception/utils.h
@@ -0,0 +1,29 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include <chrono>
+#include <ratio>
+#include <ctime>
+
+class high_resolution_timer {
+public:
+    void start(void) {
+        start_time = std::chrono::high_resolution_clock::now();
+        cur_time = std::chrono::high_resolution_clock::now();
+    }
+
+    void cp(void) {
+        cur_time = std::chrono::high_resolution_clock::now();
+    }
+
+    double get_duration(void) {
+        std::chrono::duration<double> time_elapsed = std::chrono::duration_cast<std::chrono::duration<double> >(cur_time - start_time);
+        return time_elapsed.count();
+    }
+
+private:
+    std::chrono::high_resolution_clock::time_point start_time;
+    std::chrono::high_resolution_clock::time_point cur_time;
+};
+
+#endif
