5df6e315682383307cae99f3e4c30f785aadfb9f
Roger Q <twofs0.5@gmail.com>
2018-07-15 22:09:20 +0800

    fire detection tested

--- examples/fire_detect.cpp
+++ examples/fire_detect.cpp
@@ -9,8 +9,13 @@ int main(void) {
     int code = cv::VideoWriter::fourcc('M', 'J', 'P', 'G');
     cv::VideoWriter save_stream("fire_rune_detect.avi", code, 30, Size(IMAGE_WIDTH, IMAGE_HEIGHT));
     perception::rune_bot rune_getter(cam);
+    while (cam->is_alive()) {
+        
+    }
+    /*
     rune_getter.cam_update();
     vector<digit_t> fire_digits = rune_getter.fire_acquire_digits();
+    vector<digit_t> red_digits;
     bool detected_ = false;
     while (cam->is_alive()) {
         rune_getter.cam_update();
@@ -19,7 +24,11 @@ int main(void) {
             fire_digits = rune_getter.recognize_large_digits();
         } else {
             fire_digits = rune_getter.fire_acquire_digits();
-            if(fire_digits.size() == 9) detected_ = true;
+            if(fire_digits.size() == 9) {
+                red_digits = rune_getter.red_acquire_digits();
+                for (size_t i = 0; i < red_digits.size(); ++i) fire_digits.push_back(red_digits[i]);
+                detected_ = false;
+            }
         }
         Mat cur_frame;
         rune_getter.get_cur_frame().download(cur_frame);
@@ -33,6 +42,7 @@ int main(void) {
         cv::drawContours(cur_frame, ctrs, -1, cv::Scalar(255, 0, 0), 3);
         save_stream << cur_frame;
     }
+    */
     save_stream.release();
     return 0;
 }
